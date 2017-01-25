56ec98167176ff60cf788845e69156154e04a02b
Prateek Kacker <Prateek Kacker>
2017-01-25 10:53:26 -0700

    feat: Added naked_twins
    added naked_twins. Comments and documentations are missing.

---
+++ solution.py
@@ -0,0 +1,158 @@
+assignments = []
+rows = 'ABCDEFGHI'
+cols = '123456789'
+
+def cross(A, B):
+    "Cross product of elements in A and elements in B."
+    return [s+t for s in A for t in B]
+
+boxes = cross(rows, cols)
+row_units = [cross(r,cols) for r in rows]
+column_units = [cross(rows,c) for c in cols]
+square_units = [cross(rs,cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+unitlist = row_units + square_units + column_units
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
+
+def assign_value(values, box, value):
+    """
+    Please use this function to update your values dictionary!
+    Assigns a value to a given box. If it updates the board record it.
+    """
+    values[box] = value
+    if len(value) == 1:
+        assignments.append(values.copy())
+    return values
+
+def naked_twins(values):
+    """Eliminate values using the naked twins strategy.
+    Args:
+        values(dict): a dictionary of the form {'box_name': '123456789', ...}
+
+    Returns:
+        the values dictionary with the naked twins eliminated from peers.
+    """
+    # Find all instances of naked twins
+    # Eliminate the naked twins as possibilities for their peers
+    for i in values.keys():
+        for unit in units[i]:
+            dict_naked_twins=dict()
+            for box in unit:
+                if len(values[box])==2:
+                    dict_naked_twins[box] = values[box]
+            for twin1_keys in dict_naked_twins.keys():
+                for twin2_keys in dict_naked_twins.keys():
+                    if dict_naked_twins[twin1_keys]== dict_naked_twins[twin2_keys] and twin1_keys !=twin2_keys:
+                        print(twin1_keys,twin2_keys,dict_naked_twins[twin1_keys])
+                        for box2 in unit:
+                            print("values dictionary",box2,values[box2])
+                            if box2 != twin1_keys and box2!=twin2_keys and len(values[box2])!=1 and len(values[box2])!=0:
+                                print(values[box2],box2)
+                                values[box2]=values[box2].replace(dict_naked_twins[twin1_keys][0],"")
+                                print(values[box2],box2)
+                                values[box2]=values[box2].replace(dict_naked_twins[twin1_keys][1],"")
+                                print(values[box2],box2)
+    return values                                                    
+                                        
+def grid_values(grid):
+    """
+    Convert grid into a dict of {square: char} with '123456789' for empties.
+    Args:
+        grid(string) - A grid in string form.
+    Returns:
+        A grid in dictionary form
+            Keys: The boxes, e.g., 'A1'
+            Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
+    """
+    chars = []
+    digits = '123456789'
+    for c in grid:
+        if c in digits:
+            chars.append(c)
+        if c == '.':
+            chars.append(digits)
+    assert len(chars) == 81
+    return dict(zip(boxes, chars))
+
+
+def display(values):
+    """
+    Display the values as a 2-D grid.
+    Args:
+        values(dict): The sudoku in dictionary form
+    """
+    width = 1+max(len(values[s]) for s in boxes)
+    line = '+'.join(['-'*(width*3)]*3)
+    for r in rows:
+        print(''.join(values[r+c].center(width)+('|' if c in '36' else '')
+                      for c in cols))
+        if r in 'CF': print(line)
+    return
+        
+
+def eliminate(values):
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    for box in solved_values:
+        digit = values[box]
+        for peer in peers[box]:
+            values[peer] = values[peer].replace(digit,'')
+    return values
+
+def only_choice(values):
+    for unit in unitlist:
+        for digit in '123456789':
+            dplaces = [box for box in unit if digit in values[box]]
+            if len(dplaces) == 1:
+                values[dplaces[0]] = digit
+    return values
+
+def reduce_puzzle(values):
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        # Use the Eliminate Strategy
+        values = eliminate(values)
+        # Use the Only Choice Strategy
+        values = only_choice(values)
+        #Naked Twins strategy
+        values = naked_twins(values)
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
+
+def solve(grid):
+     return search(grid)
+
+def search(values):
+    values = reduce_puzzle(values)
+    if values is False:
+        return False ## Failed earlier
+    if all(len(values[s]) == 1 for s in boxes): 
+        return values ## Solved!
+    # Chose one of the unfilled square s with the fewest possibilities
+    n,s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+    # Now use recurrence to solve each one of the resulting sudokus, and 
+    for value in values[s]:
+        new_sudoku = values.copy()
+        new_sudoku[s] = value
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
+
+
+if __name__ == '__main__':
+    diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
+    display(solve(grid_values(diag_sudoku_grid)))
+
+    try:
+        from visualize import visualize_assignments
+        visualize_assignments(assignments)
+    except:
+        print('We could not visualize your board due to a pygame issue. Not a problem! It is not a requirement.')
